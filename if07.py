8a261689e4f1fd824de1e8ed1ccc2fe7e8e3b825
fame <famephp@gmail.com>
2022-10-26 16:44:26 +0500

    07_go

--- if07.py
+++ if07.py
@@ -6,10 +6,24 @@ def main(a):
     "negative odd number",
     "negative even number",
     "the number is zero"
-
     Args:
         a: integer
     Returns:
         string: the message to print
     """
-    return
+    if a > 0:
+        if a % 2 == 0:
+            print (a, " positive even number ")
+        if a % 2 != 0:
+            print (a, " positive odd number ")
+
+    if a < 0:
+        if a % 2 == 0:
+            print (a, " negative even number ")
+        if a % 2 != 0:
+            print (a, " negative odd number ")
+
+    if a == 0:
+        print("0")
+q = (main(0))
+    
